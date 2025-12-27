74fffd7b71157872439996e6db16b660b7f2509b
Le, Thi Linh Chi <linhchi1152004@gmail.com>
2025-12-27 22:22:28 +0100

    Rate limit added

--- src/server.js
+++ src/server.js
@@ -2,10 +2,21 @@ import express from 'express';
 import userRoutes from './user.routes';
 import mainRoutes from "./main.routes";
 import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 const port = 3000;
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+    // store: ... , // Redis, Memcached, etc. See below.
+})
+
+app.use(limiter);
 
 //express.json: middleware, allow to read json from the request body
 app.use(express.json());
